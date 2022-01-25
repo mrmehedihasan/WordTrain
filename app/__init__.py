1c8f0a1d56339784348165579e551ba07465606b
Mehedi Hasan <mehedihasan2002.myc@gmail.com>
2022-01-25 19:43:28 +0600

    form complete done

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "secret"
     db.init_app(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///text.db"
     from .views import views
     app.register_blueprint(views, url_prefix='/')
 
+    with app.app_context():
+        db.create_all()
+
     return app
-
-
--- app/views.py
+++ app/views.py
@@ -1,17 +1,26 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for
 from .models import Words
-
+from . import db
+from datetime import datetime
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def index():
-    return render_template('layout.html')
+    return render_template('home.html')
 
 @views.route('/add', methods=['GET', 'POST'])
 def addWord():
     
+    if request.method == 'POST':
+        word = request.form.get('word')
+        meaning = request.form.get('meaning')
+        note = request.form.get('note')
+        NewWord = Words(word=word, meaning=meaning, note=note, added=datetime.now())
+        db.session.add(NewWord)
+        db.session.commit()
+
+
     return render_template('add.html')
     
-    if request.method == 'POST':
-        NewWord = Words()
+
     
